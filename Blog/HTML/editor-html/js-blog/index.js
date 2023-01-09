b3b8280fe9256082bbdc94033be25a1c1626f044
HungVo19 <vohung193@gmail.com>
2023-01-09 14:13:37 +0700

    90% create blog done

--- Blog/HTML/editor-html/js-blog/index.js
+++ Blog/HTML/editor-html/js-blog/index.js
@@ -1,14 +1,22 @@
 function startIndexPage() {
-    getBlogs(0);
     getTags();
     getLatestBlogs();
     checkLogin();
+    getBlogs(0);
+    $("#data-search-table").hide();
+    $("#alerttt").hide();
+
     event.preventDefault();
 }
 
 function checkLogin() {
+    drawNavigationBarr();
+    event.preventDefault();
+}
+
+function drawNavigationBarr() {
     let content = "";
-    if(sessionStorage.getItem("userId") == null) {
+    if (sessionStorage.getItem("userId") == null) {
         content = `<img alt="" src="images/site/default-avatar.jpg"
                              class="avatar avatar-44 photo"
                              height="35" width="35" style="border-radius: 50%">
@@ -16,17 +24,22 @@ function checkLogin() {
                             <li><a href="login-register.html">Login/Register</a></li>
                         </ul>`
     } else {
-        if (sessionStorage.getItem("userRole") === 1) {
-            content = `<img alt="" src="images/site/testo-02.jpg"
+        let userAvatarr = findAvatarUserByUserId(sessionStorage.getItem("userId"));
+        let userNamee = findUserNameByUserId(sessionStorage.getItem("userId"));
+        if (sessionStorage.getItem("userRole") == 1) {
+            content = `<img alt="" src="${userAvatarr}"
                              class="avatar avatar-44 photo"
                              height="35" width="35" style="border-radius: 50%">
                         <ul>
+                 
+                            <li><a >Hi ${userNamee}</a></li>
+                            <li><a href="post-blog.html">Post New Blog</a></li>
                             <li><a href="blog-simple.html">Profile</a></li>
                             <li><a href="" onclick="toUserPage()">Manage Post</a></li>
                             <li><a href="" onclick="logOut()">Logout</a></li>
                         </ul>`
         } else {
-            content = `<img alt="" src="images/site/testo-03.jpg"
+            content = `<img alt="" src="${userAvatarr}"
                              class="avatar avatar-44 photo"
                              height="35" width="35" style="border-radius: 50%">
                         <ul>
@@ -39,6 +52,34 @@ function checkLogin() {
     $("#checkLogin").html(content);
 }
 
+function findAvatarUserByUserId(id) {
+    let avatar = null;
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/users/" + id,
+        dataType: "json",
+        async: false,
+        success: function (data) {
+            avatar = data.avatar;
+        }
+    })
+    return avatar;
+}
+
+function findUserNameByUserId(id) {
+    let nameee = null;
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/users/" + id,
+        dataType: "json",
+        async: false,
+        success: function (data) {
+            nameee = data.username;
+        }
+    })
+    return nameee;
+}
+
 function toAdminPage() {
     event.preventDefault();
     window.location.href = "../default/ltr/admin-user.html";
@@ -49,12 +90,7 @@ function toUserPage() {
     event.preventDefault();
 }
 
-function loginOrRegister() {
-    $(".homePage").hide();
-    event.preventDefault()
-}
-
-function logOut(){
+function logOut() {
     sessionStorage.removeItem("userId");
     window.location.href = "index.html";
 }
@@ -92,7 +128,6 @@ function displayGetOlderPost(pageNumber) {
 <a href=" " onclick="getOlderPost(${pageNumber})">
 <span class="meta-nav">←</span> Older posts</a></div>`
     document.getElementById("getMorePosts").innerHTML = getMorePost;
-
 }
 
 function getOlderPost(page) {
@@ -112,7 +147,7 @@ function displayBlogs(data) {
         let blogRow = `<article class="hentry post">
 
                                 <header class="entry-header">
-                                    <h1 class="entry-title"><a href="../blog-single.html">${blog.title}</a></h1>
+                                    <h1 class="entry-title"><a href="" onclick="readBlogDetail(${blog.id})">${blog.title}</a></h1>
                                    
                                     <div class="entry-meta">
                                         <span class="entry-date">
@@ -154,13 +189,14 @@ function displayBlogs(data) {
 
 function readBlogDetail(blogId) {
     sessionStorage.setItem("blogId", blogId);
-    location
+    window.location.href = "blog-single.html";
+    event.preventDefault();
 }
 
 function displayLatestBlogs(blog) {
     $('.recentPostSmellCode').empty();
     $.each(blog, (i, data) => {
-        let content = `<li><a href="">${data.title}</a></li>`;
+        let content = `<li><a href="" onclick="readBlogDetail(${data.id})">${data.title}</a></li>`;
         $('.recentPostSmellCode').append(content);
     })
 }
@@ -188,16 +224,96 @@ function getTags() {
         success: function (data) {
             $('#tagCloudSideBar').empty();
             $.each(data.content, (i, tag) => {
-                let tagSidebar = `<a href="#" style="font-size: 22pt;">${tag.name}</a>`;
+                let tagSidebar = `<a href="" onclick="getBlogsByLabelIdd(${tag.id},'${tag.name}')" style="font-size: 22pt;">${tag.name}</a>`;
                 $('#tagCloudSideBar').append(tagSidebar);
             })
 
             $('#tagCloudFooter').empty();
             $.each(data.content, (i, tag) => {
-                let tagSidebar = `<a href="#" style="font-size: 22pt;">${tag.name}</a>`;
+                let tagSidebar = `<a href="" onclick="getBlogsByLabelIdd(${tag.id})" style="font-size: 22pt;">${tag.name}</a>`;
                 $('#tagCloudFooter').append(tagSidebar);
                 return i < 3;
             })
         }
     })
+    event.preventDefault();
+}
+
+function getBlogsByLabelIdd(id,namee) {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/labels/" + id + "/blogs",
+        dataType: "json",
+        success: function (data) {
+            $("#data-table").hide();
+            $("#getMorePosts").hide();
+            // $("html, body").animate({scrollTop: 0}, 1000)
+            window.scrollTo(0, 0);
+            showResultFilterByBlogId(data.totalElements, namee)
+            $('#data-search-table').show();
+            displayBlogsFindByLabelId(data.content);
+        }
+    })
+    event.preventDefault();
+}
+
+function showResultFilterByBlogId(number, tagNaem) {
+    $("#alerttt").empty();
+    let content = `<div class="alert">"There are  ${number} results with  ${tagNaem}"</div>`
+    $("#alerttt").append(content);
+    $("#alerttt").show();
+    event.preventDefault();
+}
+
+function displayBlogsFindByLabelId(data) {
+    $('#data-search-table').empty();
+    $.each(data, (i, blog) => {
+        let totalComment = getTotalCommentsEachBLog(blog.id);
+        let date = new Date(blog.createdDate);
+        let month = date.toLocaleString('default', {month: 'short'});
+        let day = date.getUTCDate();
+        let year = date.getFullYear();
+        let formattedDate = month + " " + day + " , " + year;
+
+        let blogRow = `<article class="hentry post">
+
+                                <header class="entry-header">
+                                    <h1 class="entry-title"><a href="" onclick="readBlogDetail(${blog.id})">${blog.title}</a></h1>
+                                   
+                                    <div class="entry-meta">
+                                        <span class="entry-date">
+                                        <time class="entry-date"
+                                                  datetime="2014-07-13T04:34:10+00:00">${formattedDate}</time>
+                                        </span>
+                                        <span class="comment-link">
+                                            ${totalComment} comments
+                                        </span>
+                                        <span class="cat-links">
+                                            <a rel="category tag">${blog.user.username}</a>
+                                        </span>
+                                    </div>
+                                    
+                                </header>
+                                
+                                <div class="featured-image">
+                                    <a href="../blog-single.html">
+                                        <img src="${blog.image}" alt="blog-image">
+                                    </a>
+                                </div>
+                                
+                                <div class="entry-content">
+                                    <p>${blog.description}</p>
+                                    <p>
+                                        <span class="more">
+                                            <a href="" onclick="readBlogDetail(${blog.id})" class="more-link">Continue reading <span
+                                                    class="meta-nav">→</span></a>
+                                        </span>
+                                    </p>
+                                </div>
+                                <!-- .entry-content -->
+                            </article>
+                            <!-- .hentry -->`;
+        $('#data-search-table').append(blogRow);
+    })
+    event.preventDefault();
 }
